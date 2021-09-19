cbfd32785547ca37f6ce95a8a00d1d84c1cc71a9
comradeRedShadow <90885695+comradeRedShadow@users.noreply.github.com>
2021-09-19 06:25:49 +0000

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,86 @@
+const c = new fabric.Canvas("canvas");
+
+let canvas_config = {width: 400, height: 400, backgroundColor: '#ffffff'}
+
+c.setDimensions(canvas_config);
+
+// const c = canvas.getContext("2d");
+
+function drawImage(build_in=false, num) 
+{
+    let fReader = new FileReader();
+    // create image
+    let img = new Image();
+
+    
+    img.onload = function(){
+        // create image to display
+        let img_ = new fabric.Image(img, {
+            selectable: true,
+            scaleX: 1, scaleY: 1,
+        })
+        // display image
+
+        img_.scaleToHeight(300)
+        c.add(img_);
+        c.centerObject(img_);
+        c.renderAll();
+    }
+
+    if(build_in == true)
+    {
+        fetch(`./images/${num}`).then((data) => {
+            return data.arrayBuffer()
+        }).then((data) => {
+            fReader.readAsArrayBuffer(data)
+        })
+    }
+    else {fReader.readAsDataURL(custom_up.files[0])}
+
+    fReader.onloadend = (event) =>{
+        // load image
+        img.src = event.target.result;
+    }
+}
+
+// up and down text
+let text;
+let text2;
+
+let default_text = {
+        width: canvas_config.width - 100,
+        fontFamily: "Arial",
+        editable: true,
+        left: 50,
+        right: 50,
+        textAlign: "center",
+        fontSize: 20
+}
+
+let text_margin = 5
+
+// Up
+let up_text_style = {}
+Object.assign(up_text_style, default_text)
+up_text_style.top = text_margin
+
+// Down
+let down_text_style = {}
+Object.assign(down_text_style, default_text);
+down_text_style.top = canvas_config.height - down_text_style.fontSize - text_margin;
+
+
+text = new fabric.Textbox("အပေါ်ကစာ", up_text_style)
+c.add(text);
+
+text2 = new fabric.Textbox("အောက်ကစာ", down_text_style)
+c.add(text2);
+c.renderAll();
+
+function addText(style) {
+    txt = new fabric.Textbox("စာ", style)
+    c.add(txt);
+    c.centerObject(txt)
+    c.renderAll()
+}
+
---
+++ main.js
@@ -0,0 +1,102 @@
+const custom_or_buildin_template = document.getElementById('custom_or_buildin_template');
+const choose_style = document.getElementById("choose_style");
+const font_size = document.getElementById("font_size");
+const justify = document.getElementById("justify");
+const buildin_templates = document.getElementById("buildin_templates");
+
+const up_text = document.getElementById("up_text");
+const down_text = document.getElementById("down_text")
+
+const image_to_save = document.getElementById("image_to_save");
+
+// buildin and custom
+const custom_up = document.getElementById('custom_up');
+
+
+function choose_style_fun() {
+
+    if(choose_style.style.display != 'block')
+    {
+        choose_style.style.display = 'block';
+        
+        font_size.value = c.getActiveObject().fontSize;
+        justify.value = c.getActiveObject().textAlign;
+    } else {choose_style.style.display = 'none';}
+    
+}
+
+function set_style() {
+    if(c.getActiveObject() == undefined)
+    {
+        default_text.fontSize = font_size.value;
+        default_text.textAlign = justify.value;
+    } else {
+        c.getActiveObject().fontSize = font_size.value;
+        c.getActiveObject().textAlign = justify.value;
+        c.renderAll();
+    }
+
+    choose_style_fun()
+}
+
+
+function show_buildin_templates()
+{
+    if(buildin_templates.style.display != 'block')
+    {
+        buildin_templates.style.display = 'block';
+    } else {
+        buildin_templates.style.display = 'none';
+    }
+}
+
+function custom_tp()
+{
+    custom_up.addEventListener("change", () => {
+        custom_or_buildin_template.style.display = 'none';
+
+        drawImage()
+
+    }, {once: true})
+    custom_up.click()
+}
+
+function choose_template()
+{
+    if(custom_or_buildin_template.style.display != 'block')
+    {
+        custom_or_buildin_template.style.display = 'block';
+    } else {
+        custom_or_buildin_template.style.display = 'none';
+    }
+}
+
+// canvas interaction
+
+// upper text
+up_text.addEventListener("keyup", (event) => {
+    
+    text.text = up_text.value;
+    c.renderAll();
+})
+
+down_text.addEventListener("keyup", (event) => {
+    
+    text2.text = down_text.value;
+    c.renderAll();
+})
+
+function saveImage()
+{
+    let image = new Image();
+    image.src = c.toDataURL("image/png");
+    
+    image_to_save.onload = image_to_save.setAttribute("href", image.src);
+    image_to_save.click()
+
+}
+
+function removeObject()
+{
+    c.remove(c.getActiveObject())
+}
