cc26b49284bbfceac3748b3c5cd56403a7f58a1a
comradeRedShadow <90885695+comradeRedShadow@users.noreply.github.com>
2021-09-26 08:50:29 +0000

    Update main.js

--- main.js
+++ main.js
@@ -123,21 +123,10 @@ down_text.addEventListener("keyup", (event) => {
 async function saveImage()
 {   
     let image = new Image();
-    let watermarked_image = new Image();
-
-    let hc = hidden_canvas.getContext("2d");
-    hidden_canvas.width = 577;
-    hidden_canvas.height = 483;
-
-    watermarked_image.onload = () => {
-        image_to_save.setAttribute("href", watermarked_image.src);
-        image_to_save.click()
-    }
     image.onload = () => {
         c.setWidth(canvas_config.width);
         c.setHeight(canvas_config.height);
 
-        /*
         image_to_save.setAttribute("href", image.src);
         image_to_save.click();
 
@@ -145,25 +134,6 @@ async function saveImage()
         c.setWidth(canvas_config.width);
         c.setHeight(canvas_config.height);
         upanddown();
-        */
-
-       // add watermark
-       let fReader = new FileReader();
-       fetch('./images/watermark.png')
-       .then(data => {return data.blob()})
-       .then(response => {
-            fReader.onloadend = (event) => {
-                let wm = new Image();
-                wm.src = event.target.result;
-                hc.drawImage(image, 0, 0, hidden_canvas.width, hidden_canvas.height - 50);
-                hc.drawImage(wm, 0, 433, hidden_canvas.width, 50)
-                watermarked_image.src = hidden_canvas.toDataURL("image/jpeg");
-            }
-
-            fReader.readAsDataURL(response);
-       })
-       
-       upanddown();
     }
 
 
